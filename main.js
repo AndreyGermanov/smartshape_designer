d8d03c5e840372270710576fea866e04214e5f5e
Andrey Germanov <andrey@germanov.dev>
2022-09-27 19:28:15 +0300

    Issue #9

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+import "admin-lte/dist/css/adminlte.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import './style.css'
+import "admin-lte/dist/js/adminlte.min";
+import {injectAll} from "./src/utils/html_include";
+
+document.addEventListener("DOMContentLoaded", async() => {
+    document.body.querySelector(".wrapper").style.display = '';
+    await injectAll()
+})
---
+++ src/utils/html_include.js
@@ -0,0 +1,33 @@
+/**
+ * Function injects specified HTML file to specified HTML
+ * node of the current file
+ *
+ * @param filePath - a path to a source HTML file to inject
+ * @param elem - an HTML element to which this content will
+ * be injected
+ */
+export const injectHTML = async(filePath,elem) => {
+    try {
+        const response = await fetch(filePath);
+        if (!response.ok) {
+            return;
+        }
+        elem.innerHTML = await response.text();
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+/**
+ * Function used to process all HTML tags of the following
+ * format: <div include="<filename>"></div>
+ *
+ * This function injects a content of <filename> to
+ * each div with the "include" attribute
+ */
+export const injectAll = async () => {
+    document.querySelectorAll("[include]")
+        .forEach(async (elem) => {
+            await injectHTML(elem.getAttribute("include"),elem);
+        })
+}
